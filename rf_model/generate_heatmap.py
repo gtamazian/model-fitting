58daf5ba03b03fcb2da4fe90ac50ba9fd01fbb6f
Gaik Tamazian <gaikt@mail.tau.ac.il>
2026-09-22 05:43:46 +0300

    Add command-line options for dendrogram figure file

--- rf_model/generate_heatmap.py
+++ rf_model/generate_heatmap.py
@@ -4,6 +4,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
+from dataclasses import dataclass
+
+
+@dataclass
+class FigureParams:
+    """Output figure parameters."""
+
+    width: int
+    height: int
+    resolution: int
+    output_format: str
 
 
 def read_single_features(path: str) -> list[str]:
@@ -14,7 +25,7 @@ def read_single_features(path: str) -> list[str]:
     return [k[0] for k in lines[1:] if float(k[1]) == 1.0]
 
 
-def generate_plot(hits, single_features, output_path):
+def generate_plot(hits, single_features, fig_params, output_path):
     train_data = np.log2(hits.drop(["label"], axis=1) + 1)
     number_of_samples, number_of_features = train_data.shape
 
@@ -33,6 +44,7 @@ def generate_plot(hits, single_features, output_path):
 
     cm = sns.clustermap(
         train_data,
+        figsize=(fig_params.width, fig_params.height),
         cmap="Blues",
         row_colors=row_colors,
         col_colors=col_colors,
@@ -47,7 +59,12 @@ def generate_plot(hits, single_features, output_path):
     cm.ax_col_dendrogram.set_title(
         f"A heat-map of the significance of the top {number_of_features} discriminatory motifs"
     )
-    cm.savefig(f"{output_path}.svg", format="svg", bbox_inches="tight", dpi=300)
+    cm.savefig(
+        f"{output_path}.{fig_params.output_format}",
+        format=fig_params.output_format,
+        bbox_inches="tight",
+        dpi=fig_params.resolution,
+    )
     plt.close()
 
 
@@ -55,15 +72,26 @@ def generate_plot(hits, single_features, output_path):
 @click.argument("hits_path")
 @click.argument("output_path")
 @click.option("--single_feature_path")
+@click.option("--width", default=8, type=int)
+@click.option("--height", default=8, type=int)
+@click.option("--resolution", default=300, type=int)
+@click.option("--output-format", default="svg")
 def generate_heatmap(
-    hits_path: str, output_path: str, single_feature_path: str | None
+    hits_path: str,
+    output_path: str,
+    single_feature_path: str | None,
+    width: int,
+    height: int,
+    resolution: int,
+    output_format: str,
 ) -> None:
     hits = pd.read_csv(hits_path, index_col="sample_name")
     if single_feature_path is not None:
         single_features = read_single_features(single_feature_path)
     else:
         single_features = None
-    generate_plot(hits, single_features, output_path)
+    fig_params = FigureParams(width, height, resolution, output_format)
+    generate_plot(hits, single_features, fig_params, output_path)
 
 
 if __name__ == "__main__":
